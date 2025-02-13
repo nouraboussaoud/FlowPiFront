7052910e44dec5dbf6556ecc133ec6f7996aa133
nouraboussaoud <nour.aboussaoud@esprit.tn>
2025-02-13 21:51:40 +0100

    Integration template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterPage from "./component/auth/RegisterPage";
+
+function App() {
+  return (
+    <div className="login">
+      <RegisterPage />
+    </div>
+  );
+}
+
+export default App;
