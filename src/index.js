b95fcdeeb0ffe8382f709017a75f5ec9153d1df6
Douaa Ghofrane Hadidi <douaaghofrane.hadidi@esprit.tn>
2025-02-18 11:21:06 +0100

    Google with the correct display style.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Auth0Provider
+    domain="dev-hl88xul2douspmjg.us.auth0.com"
+    clientId="LArlweZyLFfbmGvianJzRxJUDjpVs7Qk"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>
 );
 
